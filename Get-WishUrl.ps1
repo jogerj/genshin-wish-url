f78cad64ca17aa1613432ecfc44d0a9758a55d3e
jogerj <30559735+jogerj@users.noreply.github.com>
2024-08-31 12:12:31 +0200

    feat: version 0.14.0

---
+++ Get-WishUrl.ps1
@@ -0,0 +1,237 @@
+<#
+.SYNOPSIS
+  This script extracts the latest wish URL from Genshin Impact ($([char]0x539f)$([char]0x795e)) cache file.
+.DESCRIPTION
+  Usage:
+  1. Run Genshin Impact and open the "Wish History" page at least once.
+  2. Run this script to extract the latest wish URL from the cache file.
+  3. The wish URL will be copied to the clipboard.
+.EXAMPLE
+  # On Windows
+  PS C:\Users\Alice> .\get_wish_url_from_cache.ps1
+
+  # Directly from github
+
+.NOTES
+  Version:  0.14.0
+  Author:   jogerj
+  License:  MIT
+#>
+
+function Get-ValidatedWishUrl {
+  param (
+    [string]$WishUrl
+  )
+  # Character wish banner, latest 5 pulls
+  $WishUrl = "$WishUrl&page=1&size=5&gacha_type=301"
+  # If webpage, rewrite URL to call API directly
+  if ($WishUrl -match "hk4e_global") {
+    # Global
+    $WishUrl = $WishUrl -replace "https:\/\/gs.hoyoverse.com.+html\?", "https://public-operation-hk4e-sg.hoyoverse.com/gacha_info/api/getGachaLog?"
+  }
+  else {
+    # CN
+    $WishUrl = $WishUrl -replace "https:\/\/webstatic.+html\?", "https://public-operation-hk4e.mihoyo.com/gacha_info/api/getGachaLog?"
+  }
+  # Check validity
+  try {
+    $urlResponse = Invoke-RestMethod -URI $WishUrl -ContentType 'application/json' -Method Get
+    # Test OK
+    if ($urlResponse.message -ne "OK") {
+      Write-Host "URL found but it is expired/invalid! Open `"Wish History`" page again to fetch a new URL!" -ForegroundColor Yellow
+      return $null
+    }
+    return $WishUrl
+  }
+  catch {
+    Write-Host "Could not validate wish URL! Make sure you have a working internet connection." -ForegroundColor Red
+    return $null
+  }
+}
+
+function Get-GenshinLogPath {
+  $logPathGlobal = [System.Environment]::ExpandEnvironmentVariables("%userprofile%/AppData/LocalLow/miHoYo/Genshin Impact/output_log.txt")
+  $logPathChina = [System.Environment]::ExpandEnvironmentVariables("%userprofile%/AppData/LocalLow/miHoYo/$([char]0x539f)$([char]0x795e)/output_log.txt")
+  $globalExists = Test-Path $logPathGlobal
+  $cnExists = Test-Path $logPathChina
+
+  function Write-FoundGlobal { Write-Host "Found Genshin Impact (Global) log file!" -ForegroundColor Green }
+  function Write-FoundCN { Write-Host "Found $([char]0x539f)$([char]0x795e) (CN) log file!" -ForegroundColor Green }
+
+  if ($globalExists) {
+    if ($cnExists) {
+      # both exists, pick newest one
+      if ((Get-Item $logPathGlobal).LastWriteTime -ge (Get-Item $logPathChina).LastWriteTime) {
+        Write-FoundGlobal
+        return $logPathGlobal
+      }
+      else {
+        Write-FoundCN
+        return $logPathChina
+      }
+    }
+    else {
+      Write-FoundGlobal
+      return $logPathGlobal
+    }
+  }
+  else {
+    if ($cnExists) {
+      Write-FoundCN
+      return $logPathChina
+    }
+    else {
+      return $null
+    }
+  }
+}
+
+function Invoke-RunAsAdminPrompt {
+  if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "Do you want to try to run the script as Administrator? Press [ENTER] to continue, or any key to cancel." -ForegroundColor DarkBlue
+    $keyInput = [Console]::ReadKey($true).Key
+    if ($keyInput -ne "13") {
+      return
+    }
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList "-noexit $arguments $reg"
+  }
+  else {
+    Write-Host "Make sure Genshin Impact is installed and open `"Wish History`" page at least once." -ForegroundColor Red
+  }
+}
+
+function Stop-BrowserProcessSilently {
+  [CmdletBinding(SupportsShouldProcess)]
+  param ()
+  $retryLimit = 5
+  $retryCount = 0
+  while ($retryCount -lt $retryLimit) {
+    $zfBrowser = Get-Process -Name "ZFGameBrowser" -ErrorAction SilentlyContinue
+    if ($zfBrowser) {
+      $zfBrowser | Stop-Process -Force
+      Start-Sleep 2
+      $retryCount++
+    }
+    else {
+      break
+    }
+  }
+  if ($retryCount -gt 0) {
+    # if retried, then browser was running
+    Write-Host "Waiting for in-game browser process to close..." -ForegroundColor Yellow
+    # Wait for file lock to release
+    Start-Sleep 10
+  } # else no need to wait
+}
+
+function Remove-CacheFolder {
+  [CmdletBinding(SupportsShouldProcess)]
+  param (
+    [string]$CacheFolderPath
+  )
+  $retryLimit = 5
+  $retryCount = 0
+  while ((Test-Path "$CacheFolderPath") -and ($retryCount -lt $retryLimit)) {
+    Remove-Item -Recurse -Force "$CacheFolderPath" -ErrorAction SilentlyContinue
+    $retryCount++
+    Start-Sleep 2
+  }
+}
+
+function Invoke-PromptClearCache {
+  param (
+    [string]$CacheFolderPath
+  )
+  Write-Host "Do you want to try to clear the cache folder? This will require restarting Genshin Impact if the game is running.
+(This will force the game to generate a new cache file)
+Press [ENTER] to continue, or any key to cancel." -ForegroundColor DarkBlue
+  $keyInput = [Console]::ReadKey($true).Key
+  if ($keyInput -eq "13") {
+    # kill browser if Genshin is running
+    Stop-BrowserProcessSilently
+    # remove webCaches folder to force game to generate new cache
+    Remove-CacheFolder "$CacheFolderPath"
+    Write-Host "Cache folder cleared! Please restart Genshin Impact and reopen `"Wish History`"!" -ForegroundColor Red
+  }
+  else {
+    Write-Host "Please reopen `"Wish History`" to fetch new URL!" -ForegroundColor Red
+  }
+}
+
+function Get-GenshinWishUrl {
+  $LogPath = Get-GenshinLogPath
+  if (-not $LogPath) {
+    Write-Host "Cannot find Genshin Impact log file! Make sure to run Genshin Impact and open the wish history at least once!" -ForegroundColor Red
+    Invoke-RunAsAdminPrompt
+    return $null
+  }
+
+  $LogsContent = Get-Content -Path $LogPath
+  $GameDataPathRegexPattern = "(?m).:/.+(GenshinImpact_Data|YuanShen_Data)"
+  $LogMatch = $LogsContent -match $GameDataPathRegexPattern
+
+  if (-not $LogMatch) {
+    Write-Host "Cannot find Genshin Impact path in log file! Make sure to run Genshin Impact and open the wish history at least once!" -ForegroundColor Red
+    Pause
+    return $null
+  }
+
+  $GameDataPath = ($LogMatch | Select-Object -Last 1) -match $GameDataPathRegexPattern
+  $GameDataPath = $Matches[0]
+  $CacheFolderPath = "$GameDataPath/webCaches"
+  try {
+    if (Test-Path "$CacheFolderPath") {
+      Write-Host "Cache folder found! Trying to extract latest cache file..." -ForegroundColor Green
+      try {
+        $cacheVerPath = Get-Item (Get-ChildItem -Path "$GameDataPath/webCaches" | Sort-Object LastWriteTime -Descending | Select-Object -First 1).FullName
+        $cachePath = "$cacheVerPath/Cache/Cache_Data/data_2"
+
+        if (-not (Test-Path $cachePath)) {
+          throw "Cache file not found!"
+        }
+
+        # check cache not older than 2 days
+        if ((Get-Item $cachePath).LastWriteTime -le $(Get-Date).AddDays(-2)) {
+          throw "Cache file is too old!"
+        }
+
+        $tmpFile = "$env:TEMP/ch_data_2"
+        Copy-Item $cachePath -Destination $tmpFile
+        $content = Get-Content -Encoding UTF8 -Raw $tmpfile
+        Remove-Item $tmpFile
+
+        $splitted = $content -split "1/0/" | Select-Object -Last 1
+        $found = $splitted -match "https.+?game_biz=hk4e_(global|cn)"
+        if ($found) {
+          $cacheVer = Split-Path $cacheVerPath -Leaf
+          Write-Host "Wish URL file found in $cacheVer cache! Validating wish URL..." -ForegroundColor Green
+          $wishUrl = Get-ValidatedWishUrl $Matches[0]
+          if ($wishUrl) {
+            Write-Host "Wish URL validated!" -ForegroundColor Green
+            Set-Clipboard -Value $WishUrl
+            Write-Host "URL copied to clipboard, paste it on any Genshin Impact ($([char]0x539f)$([char]0x795e)) wish tracker`nor Ctrl-Click to open in web browser." -ForegroundColor Green
+            return $wishUrl
+          }
+        }
+        throw "No valid URL found in cache!"
+      }
+      catch {
+        Write-Host "Encountered error: "$_.Exception.Message -ForegroundColor Yellow
+        Write-Host "Cache folder was found but could not find valid wish URL in the files!" -ForegroundColor Yellow
+        Invoke-PromptClearCache "$CacheFolderPath"
+      }
+    }
+    else {
+      throw "Cache folder was not found! Please restart Genshin Impact and reopen `"Wish History`"!"
+    }
+  }
+  catch {
+    Write-Host "Encountered error: "$_.Exception.Message -ForegroundColor Red
+    Pause
+  }
+  return $null
+}
+
+# Run the script
+Get-GenshinWishUrl
--- README.md
+++ README.md
@@ -1,91 +1,118 @@
-# Usage
+# genshin-wish-url
+
+## Usage
+
 1. Win+R and open `powershell` (or `pwsh` if Powershell 6+ installed)
 2. Paste and run following
+
 * All versions (Global/China)
+
 ```powershell
 Set-ExecutionPolicy Bypass -Scope CurrentUser -Force;
-iex (irm 'https://gist.githubusercontent.com/jogerj/0339e61a92e0de2e360c5212a94854e8/raw/get_wish_url_from_cache.ps1')
-```
-* \*NEW\* shortened URL to redirect here
-```powershell
-Set-ExecutionPolicy Bypass -Scope CurrentUser -Force;
-iex (irm 'https://genshin.jogerj.com/wishurl')
+iex (irm 'https://github.com/jogerj/genshin-wish-url/raw/main/Get-WishUrl.ps1')
 ```
 
 ### [Click here for getting wish URL from Genshin Impact on Android](https://gist.github.com/jogerj/2372d0e5bee51e001a6d8956240d527b)
 
-# Report bugs/errors
+## Report bugs/errors
+
 Comment on this gist or send message on [paimon.moe Discord server](https://discord.com/channels/820601523125747712/820601523125747715/1012175730873991228)
 
 Officially supported: **Powershell 5.1 and above** (Default installed on **Windows 10 and above**). Older OS will not be supported, but you may try [upgrading your powershell version](https://www.microsoft.com/en-us/download/details.aspx?id=54616).
 
-# Changelog
-<details> 
+## Changelog
 
-## Version 0.13.0
+<details>
+
+### Version 0.14.0
+
+* New repository
+* Major refactoring of code
+* Support deletion of webCaches when stuck
+
+### Version 0.13.0
+
 * Fix for new API URL in 4.8 (Global)
 * Rework checks. Now returned URL defaults to Character Wish Banner
 
-## Version 0.12.2
+### Version 0.12.2
+
 * Fix for new API URL in 4.6 (CN)
 * Added short URL
- 
-## Version 0.12.1
+
+### Version 0.12.1
+
 * Typo message fix
-   
-## Version 0.12.0
+
+### Version 0.12.0
+
 * Deprecated and removed fallback methods
 * Now cache path lookup checks for latest modified subfolder
-   
-## Version 0.11.1
+
+### Version 0.11.1
+
 * Fix for Genshin 4.0
-   
-## Version 0.11.0
+
+### Version 0.11.0
+
 * Fix for Genshin 3.8
 
-## Version 0.10.0
+### Version 0.10.0
+
 * Now if a user has both global and china version of the game, it will load the URL from whichever is last open.
 
-## Version 0.9.0
+### Version 0.9.0
+
 * Fix CN suffix to `game_biz=hk4e_cn`
 * Fix check validity for URLs beginning with `https://webstatic...`
 
-## Version 0.8.0
+### Version 0.8.0
+
 * Added new method from [MadeBaruna](https://gist.github.com/MadeBaruna/1d75c1d37d19eca71591ec8a31178235/). Now supports 3 different methods (should be totally foolproof 🤞)
 * Automatically checks for expired/invalid link
 * ~~URL date is now retrieved from URL `timestamp` parameter~~ Removed URL time since it's unnecessary to check for URL expiry
 
+### Version 0.7.0
 
-## Version 0.7.0
 * Combined Global and China server scripts. Now will check for Global first before China log files. Can be overriden to force check China server by adding `china` to the parameter like this:
+
    ```powershell
    powershell iex "&{$(irm 'https://gist.githubusercontent.com/jogerj/0339e61a92e0de2e360c5212a94854e8/raw/get_wish_url_from_cache.ps1')} china"
    ```
+
 * Pass on args to elevated powershell correctly
 * Use more accurate file path pattern from [here](https://gist.github.com/MadeBaruna/1d75c1d37d19eca71591ec8a31178235/)
   
-## Version 0.6.0
+### Version 0.6.0
+
 * Added back old method as fallback option (when webCache gets destroyed/new install)
 
 ### Version 0.5.0
+
 * Changed game path lookup to search in log file instead of install path
 * Added China version (needs testing)
 * adjusted URL lookup pattern
 
 ### Version 0.4.0
+
 * ChromeCacheView no longer needed. Script will now read cache files directly
 * Credits to @PrimeCicada for finding an alternate path
   
 ### Version 0.3.0
+
 * Added handling of different game path
 * Fixes issue with older installs of Genshin with different path
 * Added fallback option for manual entry of game path. Drag and drop your shortcut or exe file (either launcher or game works), the cache path will be grabbed correctly
   
 ### Version 0.2.0
+
 * Added date of URL to output
 * Add warning for URL older than 24h
+
 ### Version 0.1.0
+
 * Initial release
+
 </details>
 
 ## [Original script](https://gist.github.com/MadeBaruna/1d75c1d37d19eca71591ec8a31178235)
